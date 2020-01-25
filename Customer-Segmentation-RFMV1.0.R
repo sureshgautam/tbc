b556c433e80655d7930974153614515ff9607df2
Suresh Gautan <sureshgautan@Sureshs-MacBook-Pro.local>
2020-01-25 15:15:29 +0545

    Reference in each script file added.

--- Customer-Segmentation-RFMV1.0.R
+++ Customer-Segmentation-RFMV1.0.R
@@ -75,3 +75,10 @@ rfm_rf_plot(rfm_result)
 
 # RFM Analysis End here
 ###################################################
+###########################################
+# Customer Segmentation using RFM Analysis - Recency Frequency Monetry
+#
+# Reference - https://www.kaggle.com/hendraherviawan/customer-segmentation-using-rfm-analysis-r
+# Reference - https://github.com/rsquaredacademy/rfm
+#
+###########################################
